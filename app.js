c46da538b426f7152e01f9720c799eb6690eb160
Mijo Galic <gmijo47@gmail.com>
2022-11-05 22:21:33 +0100

    Initial

--- app.js
+++ app.js
@@ -1,144 +1,428 @@
 const qrcode = require('qrcode-terminal');
 const scrp = require('./scraper')
+const process = require('process');
 const fs = require('fs');
+const pack = require('./package.json')
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const {
+    Client,
+    LocalAuth
+} = require('whatsapp-web.js');
 const config = require('./config/config');
 const date = require('date-and-time');
 let dataConf = JSON.parse(fs.readFileSync('./config/data.json', 'utf8'));
 
+var days = ['Nedelja', 'Ponedeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota']
+var starttime = new Date()
+    //Create client with local auth
 const client = new Client({
     authStrategy: new LocalAuth()
 });
- 
 
-
+//Generate qr code for sign in
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, {
+        small: true
+    });
 });
 
+//When client ready scrape data from timetable
 client.on('ready', () => {
     console.log('Client is ready!');
-    if(scrp.getData()){
-     console.log("Raw data is writed, proceeding to parse");
+    if (scrp.getData()) {
+        console.log("Raw data is writed, proceeding to parse");
 
-    }else {
-      console.log("Raw data isn't writed, aborting parsing");
+    } else {
+        console.log("Raw data isn't writed, aborting parsing");
     }
 });
 
-client.on('message', async (msg) => {
+//Client commands
+client.on('message', async(msg) => {
 
-      const chat = await msg.getChat();
+    const chat = await msg.getChat();
 
-      //Register group chanell
-      if(chat.isGroup){
-        if(msg.body == "!register" && msg.author == config.super_admin.id){
+    //Register group chanell
+    if (chat.isGroup) {
+        if (msg.body == "!sub" && msg.author == config.super_admin.id) {
 
-          //Check if chanell is registered
-            if(dataConf.group.id == ""){
+            //Check if chanell is registered
+
+            if (dataConf.group.id == "") {
+
                 //Not registered
                 dataConf.group.id = chat.id._serialized;
-                try{
-                fs.writeFileSync('config/data.json', JSON.stringify(dataConf, null, "\t"));
-                msg.react("✅")
-                msg.reply("✅ - Kanal registrovan!\n Obavještenja će stizati ovdje!")
-                }catch(err){
-                  msg.reply("❌ - Došlo je do problema\n Kanal nije registrovan!")
-                  msg.react("❌");
-                  console.log(err);
+                try {
+
+                    fs.writeFileSync('config/data.json', JSON.stringify(dataConf, null, "\t"));
+                    msg.react("✅")
+                    msg.reply("✅ - Kanal registrovan!\n Obavještenja će stizati ovdje!")
+
+                } catch (err) {
+
+                    msg.reply("❌ - Došlo je do problema\n Kanal nije registrovan!")
+                    msg.react("❌");
+                    console.log(err);
+
                 }
-            }else {
-              //Registered
-              msg.reply("❌ - Neki kanal je već registrovan!\n Koristite !unregister za uklanjanje kanala.")
-              msg.react("❌");
+            } else {
+
+                //Registered
+                msg.reply("❌ - Neki kanal je već registrovan!\n Koristite !unregister za uklanjanje kanala.")
+                msg.react("❌");
+
             }
             return;
-        }else if(msg.body == "!register" || msg.body == "!unregister"  && msg.author != config.super_admin.id){
+        } else if (msg.body == "!sub" || msg.body == "!unsub" && msg.author != config.super_admin.id) {
 
-          msg.reply("⛔ - Nemate dozvolu da izvršavate dev komande");
-          msg.react("⛔")
+            msg.reply("⛔ - Nemate dozvolu da izvršavate dev komande");
+            msg.react("⛔")
 
-        }else if(msg.body == "!unregister" && msg.author == config.super_admin.id){
-          //Unregister group chanell
-          if(dataConf.group.id == ""){
-              //Chanell not registered
-              msg.reply("❌ - Došlo je do problema\n Kanal nije registrovan!");
-              msg.react("❌")
-        }else {
-          //Chanell registered
-          dataConf.group.id = "";
-          try{
-            fs.writeFileSync('config/data.json', JSON.stringify(dataConf, null, "\t"));
-            msg.react("✅")
-            msg.reply("✅ - Kanal UNregistrovan!\n Obavještenja neće stizati više ovdje!")
-          }catch(err){
-            msg.reply("❌ - Došlo je do problema\n Kanal nije UNregistrovan!")
-            msg.react("❌");
-            console.log(err);
-          }
-        }
-        return;
-      }
-    }else {
-      //Private chats
-      if(msg.body.startsWith('!')){
-        //Command
-        var days = [ 'Nedelja', 'Ponedeljak', 'Utorak', 'Srijeda', 'Cetvrtak', 'Petak', 'Subota']
-        let day;
-        switch(msg.body.substring(1, msg.body.length)){
-          case "rd":{
-            const message = [];
-            let data =  JSON.parse(fs.readFileSync(('./data/timetable_today.json')));
-            if(data.class.length == 0){
-              //No classes
-              day = new Date().getDay();
-              var title = ("❇️ ```Raspored - " + days[day] + "``` ❇️\n" + "        " + date.format(new Date(), 'DD.MM.YYYY') + "\n\n");
-              message.push(title);
-              class_data = "*Nema sati za prikazati!* "
-              message.push(class_data); 
-            }else {
-              //Have classes
-              day = date.parse(data.class[0].date, "YYYY-MM-DD").getDay();
-              let i = 0;
-              var title = ("❇️ ```Raspored - " + days[day] + "``` ❇️\n" + "        " + date.format(new Date(), 'DD.MM.YYYY') + "\n\n");
-              message.push(title);
-              data.class.forEach(function(entry) {
-                class_data = ("*"+data.class[i].name + "*\n" + " 🔸 Profesor: " + getTeacher(data.class[i].teacher) + "\n 🕐 Početak: " + data.class[i].starttime + "\n 🕐 Kraj: " + data.class[i].endtime + "\n 🚪 Sala: " + getRoom(data.class[i].classroom) + "\n\n\n");
-                message.push(class_data);
-              i++;
-              });
+        } else if (msg.body == "!unsub" && msg.author == config.super_admin.id) {
+
+            //Unregister group chanell
+            if (dataConf.group.id == "") {
+
+                //Chanell not registered
+                msg.reply("❌ - Došlo je do problema\n Kanal nije registrovan!");
+                msg.react("❌")
+
+            } else {
+
+                //Chanell registered
+                dataConf.group.id = "";
+                try {
+
+                    fs.writeFileSync('config/data.json', JSON.stringify(dataConf, null, "\t"));
+                    msg.react("✅")
+                    msg.reply("✅ - Kanal UNregistrovan!\n Obavještenja neće stizati više ovdje!")
+
+                } catch (err) {
+
+                    msg.reply("❌ - Došlo je do problema\n Kanal nije UNregistrovan!")
+                    msg.react("❌");
+                    console.log(err);
+
+                }
             }
-            msg.reply((message.toString()).replace(',', " "));
-        
-            break;
-          }w
-          default:{
-            
-            msg.reply("🆘 Nepoznata komanda, kucajte !pomoc za pomoć");
-          }
+            return;
         }
-      }else{
-        return;
-      }
+    } else {
+        //Private chats
+        if (msg.body.startsWith('!')) {
+
+            if (msg.body.split(" ").length > 1) {
+
+                //Multiple arg command
+                let args = new Array();
+                args = msg.body.split(" ");
+                switch (args[0].replace("!", "")) {
+                    case "rs":
+                        {
+
+                            //Check arg lenght
+                            if (args.length == 2) {
+
+                                //Check if date is valid, and change format
+                                try {
+
+                                    let clientDate = date.parse(args[1], "DD.MM.YYYY");
+
+                                    if (clientDate instanceof Date && !isNaN(clientDate.valueOf())) {
+
+                                        const message = [];
+
+                                        let clientFormatData = date.format(clientDate, "YYYY-MM-DD");
+                                        const data = await scrp.getDayTT(clientFormatData);
+                                        if (data.class.length == 0) {
+
+                                            //No classes
+                                            day = clientDate.getDay();
+                                            var title = ("❇️ ```Raspored - " + days[day] + "``` ❇️\n" + "        " + date.format(clientDate, 'DD.MM.YYYY') + "\n\n");
+                                            message.push(title);
+                                            class_data = "*Nema sati za prikazati!* "
+                                            message.push(class_data);
+
+                                        } else {
+
+                                            //Have classes
+                                            day = date.parse(data.class[0].date, "YYYY-MM-DD").getDay();
+                                            let i = 0;
+                                            var title = ("❇️ ```Raspored - " + days[day] + "``` ❇️\n" + "        " + date.format(clientDate, 'DD.MM.YYYY') + "\n\n");
+                                            message.push(title);
+                                            data.class.forEach(function(entry) {
+                                                class_data = ("*" + data.class[i].name + "*\n" + " 🔸 Profesor: " + getTeacher(data.class[i].teacher) + "\n 🕐 Početak: " + data.class[i].starttime + "\n 🕐 Kraj: " + data.class[i].endtime + "\n 🚪 Sala: " + getRoom(data.class[i].classroom) + "\n\n\n");
+                                                message.push(class_data);
+                                                i++;
+                                            });
+
+                                        }
+                                        msg.reply(message.join(" "));
+
+                                    } else {
+
+                                        msg.reply("❌ -Format datuma nevažeči\n Molimo koristite formad DD.MM.GGGG!")
+                                        msg.react("❌");
+
+                                    }
+
+                                } catch (err) {
+
+                                    console.log("error " + err);
+
+
+                                }
+                            } else {
+
+                                msg.reply("🆘 Previše argumenata !pomoć za pomoć");
+                                msg.react("🆘");
+
+                            }
+
+                            break;
+
+                        }
+                    default:
+                        {
+
+                            msg.reply("🆘 Nepoznata komanda, kucajte !pomoć za pomoć");
+                            msg.react("🆘")
+
+                        }
+                }
+
+            } else {
+
+                //Single arg command
+                let day;
+                switch (msg.body.substring(1, msg.body.length)) {
+                    case "rd":
+                        {
+                            const message = [];
+                            let data = JSON.parse(fs.readFileSync(('./data/timetable_today.json')));
+
+                            if (data.class.length == 0) {
+
+                                //No classes
+                                day = new Date().getDay();
+                                var title = ("❇️ ```Raspored - " + days[day] + "``` ❇️\n" + "        " + date.format(new Date(), 'DD.MM.YYYY') + "\n\n");
+                                message.push(title);
+                                class_data = "*Nema sati za prikazati!* "
+                                message.push(class_data);
+
+                            } else {
+
+                                //Have classes
+                                day = date.parse(data.class[0].date, "YYYY-MM-DD").getDay();
+                                let i = 0;
+                                var title = ("❇️ ```Raspored - " + days[day] + "``` ❇️\n" + "        " + date.format(new Date(), 'DD.MM.YYYY') + "\n\n");
+                                message.push(title);
+                                data.class.forEach(function(entry) {
+                                    class_data = ("*" + data.class[i].name + "*\n" + " 🔸 Profesor: " + getTeacher(data.class[i].teacher) + "\n 🕐 Početak: " + data.class[i].starttime + "\n 🕐 Kraj: " + data.class[i].endtime + "\n 🚪 Sala: " + getRoom(data.class[i].classroom) + "\n\n\n");
+                                    message.push(class_data);
+                                    i++;
+                                });
+
+                            }
+
+                            msg.reply(message.join(" "));
+                            break;
+                        }
+                    case "rw":
+                        {
+			
+			const message = [];
+
+			let data = JSON.parse(fs.readFileSync(('./data/timetable_week.json')));
+			let body = JSON.parse(fs.readFileSync(('./config/timetable-body.json')));
+
+			let starDateRaw = body.__args[1].datefrom
+			let endDateRaw = body.__args[1].dateto
+
+			let startDateUnp = date.parse(starDateRaw, 'YYYY-MM-DD');
+			let endDateUnp = date.parse(endDateRaw, 'YYYY-MM-DD');
+
+			var title = ("❇️     ```Tjedni raspored```     ❇️\n        " + date.format(startDateUnp, 'DD.MM.YYYY') + " - " + date.format(endDateUnp, 'DD.MM.YYYY') + "\n\n");
+			message.push(title);
+
+			for(let i = 1; i <= 6; i++){
+
+				let dayTTUnp = date.addDays(startDateUnp, i);
+				let dayTTPar = date.format(dayTTUnp, 'YYYY-MM-DD');
+
+				let subMess = "";
+				
+				let dayCount = new Array();
+				dayCount[i] = 0;
+
+				subMess = "   *🟢 " + days[i] + "*\n\n";
+
+				for(let k = 0; k < data.class.length; k++){
+
+					if(dayTTPar == data.class[k].date){
+						
+						dayCount[i]++;
+						subMess += ("      " + data.class[k].name + "\n" + "        ⏲️ " + data.class[k].starttime + " do " + data.class[k].endtime + "\n\n");
+						console.log(subMess);
+
+					
+				//	class_data = ("*" + data.class[i].name + "*\n" + " 🔸 Profesor: " + getTeacher(data.class[i].teacher) + "\n 🕐 Početak: " + data.class[i].starttime + "\n 🕐 Kraj: " + data.class[i].endtime + "\n 🚪 Sala: " + getRoom(data.class[i].classroom) + "\n\n\n");
+				     }
+
+				}
+
+				if (dayCount[i] < 1){
+					
+					subMess = subMess + "        _Nema sati za prikazati_\n\n";
+					subMess = subMess.replace("🟢 ", "🔴 ")
+				
+				}else {
+					let clientDate = date.format(dayTTUnp, "DD.MM.YYYY");
+					subMess += "        _Detalnije *!rs " + clientDate + "*_\n\n"
+				}
+
+				message.push(subMess);
+				
+				
+			}
+			/* ```Tjedni raspored```
+   			 15.11.2022  do 21.11.2022
+
+     			 *Ponedeljak - 15.12.2022*
+
+
+*/
+			msg.reply(message.join(" "));
+			break;
+                        }
+                    case "rw+":
+                        {
+			const message = [];
+
+			let data = JSON.parse(fs.readFileSync(('./data/timetable_next_week.json')));
+			let body = JSON.parse(fs.readFileSync(('./config/timetable-body-n.json')));
+
+			let starDateRaw = body.__args[1].datefrom
+			let endDateRaw = body.__args[1].dateto
+
+			let startDateUnp = date.parse(starDateRaw, 'YYYY-MM-DD');
+			let endDateUnp = date.parse(endDateRaw, 'YYYY-MM-DD');
+
+			var title = ("❇️     ```Tjedni raspored```     ❇️\n        " + date.format(startDateUnp, 'DD.MM.YYYY') + " - " + date.format(endDateUnp, 'DD.MM.YYYY') + "\n\n");
+			message.push(title);
+
+			for(let i = 1; i <= 6; i++){
+
+				let dayTTUnp = date.addDays(startDateUnp, i);
+				let dayTTPar = date.format(dayTTUnp, 'YYYY-MM-DD');
+
+				let subMess = "";
+				
+				let dayCount = new Array();
+				dayCount[i] = 0;
+
+				subMess = "   *🟢 " + days[i] + "*\n\n";
+
+				for(let k = 0; k < data.class.length; k++){
+
+					if(dayTTPar == data.class[k].date){
+						
+						dayCount[i]++;
+						subMess += ("      " + data.class[k].name + "\n" + "        ⏲️ " + data.class[k].starttime + " do " + data.class[k].endtime + "\n\n");
+						console.log(subMess);
+
+					
+				//	class_data = ("*" + data.class[i].name + "*\n" + " 🔸 Profesor: " + getTeacher(data.class[i].teacher) + "\n 🕐 Početak: " + data.class[i].starttime + "\n 🕐 Kraj: " + data.class[i].endtime + "\n 🚪 Sala: " + getRoom(data.class[i].classroom) + "\n\n\n");
+				     }
+
+				}
+
+				if (dayCount[i] < 1){
+					
+					subMess = subMess + "        _Nema sati za prikazati_\n\n";
+					subMess = subMess.replace("🟢 ", "🔴 ")
+				
+				}else {
+					let clientDate = date.format(dayTTUnp, "DD.MM.YYYY");
+					subMess += "        _Detalnije *!rs " + clientDate + "*_\n\n"
+				}
+
+				message.push(subMess);
+				
+				
+			}
+			/* ```Tjedni raspored```
+   			 15.11.2022  do 21.11.2022
+
+     			 *Ponedeljak - 15.12.2022*
+
+
+*/
+			msg.reply(message.join(" "));
+			break;
+                        }
+                    case "pomoć":
+                        {
+                            msg.reply("🆘 ```Pomoć - lista komandi``` 🆘\n\n      *!rd* - prikaz rasporeda za danas\n      *!rw* - prikaz rasporeda za tekuči tjedan\n      *!rw+* - prikaz rasporeda za nadolazeći tjedan\n      *!rs 01.01.2022* - pretraga rasporeda za određeni datum\n      *!info* - prikaz osnovnih informacija o botu\n")
+                            break;
+                        }
+                    case "info":
+                        {
+                            console.log(process.memoryUsage());
+                            console.log(process.platform);
+
+                            msg.reply("ℹ️ ```Informacije o botu``` ℹ️\n\n      *👨‍💻 Autor:* " + pack.author + "\n      *📶 Verzija:* " + pack.version + "\n      *⏱️ Uptime:* " + getUptime() + "\n      *💾 Memory usage:* " + Math.round((process.memoryUsage().rss / 1000000), 2) + "MB\n      *💻 OS:* " + (process.platform).toUpperCase() + "\n      *⚙️ RTE:* Node " + process.version + "\n      *📝 Opis:* " + pack.description + "\n")
+                            break;
+                        }
+                    default:
+                        {
+
+                            msg.reply("🆘 Nepoznata komanda, kucajte !pomoć za pomoć");
+                            msg.react("🆘")
+
+                        }
+                }
+            }
+        } else {
+            return;
+        }
     }
-      
-  
+
+
 });
 
-function getTeacher(id){
-  let teacherConf = JSON.parse(fs.readFileSync('./data/teacher.json', 'utf8'));
-  let teacher = teacherConf.data_rows[id].short
-  return teacher;
+function getTeacher(id) {
+    let teacherConf = JSON.parse(fs.readFileSync('./data/teacher.json', 'utf8'));
+    let teacher = teacherConf.data_rows[id].short
+    return teacher;
 
 }
 
-function getRoom(id){
-  let roomConf = JSON.parse(fs.readFileSync('./data/classroom.json', 'utf8'));
-  let room = roomConf.data_rows[id].short
-  return room;
+function getUptime() {
+    var ut_sec = process.uptime();
+    var ut_min = ut_sec / 60;
+    var ut_hour = ut_min / 60;
+    var ut_day = ut_min / 24;
+
+    ut_sec = Math.floor(ut_sec);
+    ut_min = Math.floor(ut_min);
+    ut_hour = Math.floor(ut_hour);
+    ut_day = Math.floor(ut_day);
+
+    ut_day = ut_day % 24;
+    ut_hour = ut_hour % 60;
+    ut_min = ut_min % 60;
+    ut_sec = ut_sec % 60;
+
+    let messgae = ut_day + " d " + ut_hour + " h " + ut_min + " m " + ut_sec + " s.";
+    return messgae;
+}
+
+function getRoom(id) {
+    let roomConf = JSON.parse(fs.readFileSync('./data/classroom.json', 'utf8'));
+    let room = roomConf.data_rows[id].short
+    return room;
 }
 
 
 client.initialize();
- 
--- scraper.js
+++ scraper.js
@@ -4,10 +4,11 @@ const config = require('./config/config');
 const date = require('date-and-time');
 
 let timetable_body;
-let teacher_body;
 let timetableday_body;
+let timetable_body_n;
 
 let timetable_data;
+let timetable_data_n;
 let tttoday;
 
 
@@ -15,22 +16,30 @@ let tttoday;
  async function getData(){
 
     console.log("Function getData() called!");
- 
-     const start = date.format(new Date("2022-10-24"), 'YYYY-MM-DD');
-     const end = date.format(new Date("2022-10-30"), 'YYYY-MM-DD');
 
      timetable_body = JSON.parse(fs.readFileSync('./config/timetable-body.json', 'utf8'));
-     teacher_body = JSON.parse(fs.readFileSync('./config/teacher-body.json', 'utf8'));
      timetableday_body = JSON.parse(fs.readFileSync('./config/timetable-body-today.json', 'utf8'));
+     timetable_body_n = JSON.parse(fs.readFileSync('./config/timetable-body-n.json', 'utf8'));
 
      await configureBody();
           
+    //This week 
     response = await fetch('https://fsre.edupage.org/timetable/server/currenttt.js?__func=curentttGetData', {
         method: 'post',
         body: JSON.stringify(timetable_body),
         headers: {'Content-Type': 'application/json'}
     });
     timetable_data = await response.json();
+
+    //Next week
+    response = await fetch('https://fsre.edupage.org/timetable/server/currenttt.js?__func=curentttGetData', {
+        method: 'post',
+        body: JSON.stringify(timetable_body_n),
+        headers: {'Content-Type': 'application/json'}
+    });
+    timetable_data_n = await response.json();
+
+    //Today
     response = await fetch('https://fsre.edupage.org/timetable/server/currenttt.js?__func=curentttGetData', {
         method: 'post',
         body: JSON.stringify(timetableday_body),
@@ -46,26 +55,86 @@ let tttoday;
 setInterval(getData, config.delay.post_request);
 
 async function configureBody(){
+
     console.log("Function configureBody() called!");
+
     const now = date.format(new Date(), 'YYYY-MM-DD');
     timetableday_body.__args[1].datefrom = now;
     timetableday_body.__args[1].dateto = now;
+
+    if (new Date().getDay() == 0){
+            //Sunday
+
+            //Current week
+            let start = date.format(new Date(), 'YYYY-MM-DD');
+            let endunf = date.addDays(new Date(), 6)
+            let end = date.format(endunf, 'YYYY-MM-DD');
+
+            //Next week
+            let start2unf = new Date();
+            let end2unf = date.addDays(start2unf, 6);
+
+            let start2 = date.format(start2unf, 'YYYY-MM-DD');
+            let end2 = date.format(end2unf, 'YYYY-MM-DD');
+            
+
+            timetable_body_n.__args[1].datefrom = start2; 
+            timetable_body_n.__args[1].dateto = end2; 
+
+            timetable_body.__args[1].datefrom = start; 
+            timetable_body.__args[1].dateto = end; 
+
+    }else {
+        //Every other day
+        let day = new Date().getDay() / -1;
+        
+        //This week
+        let startunf = date.addDays(new Date(), day);
+        let start = date.format(startunf, 'YYYY-MM-DD');
+
+        let endunf = date.addDays(startunf, 6)
+        let end = date.format(endunf, 'YYYY-MM-DD');
+
+        //Next week
+        let start2unf = date.addDays(startunf, 7);
+        let end2unf = date.addDays(endunf, 7);
+
+        let start2 = date.format(start2unf, 'YYYY-MM-DD');
+        let end2 = date.format(end2unf, 'YYYY-MM-DD');
+
+        timetable_body.__args[1].datefrom = start; 
+        timetable_body.__args[1].dateto = end; 
+
+        timetable_body_n.__args[1].datefrom = start2; 
+        timetable_body_n.__args[1].dateto = end2;
+    }
+    
     try{
+
         fs.writeFileSync('./config/timetable-body-today.json', JSON.stringify(timetableday_body, null, "\t"));
+        fs.writeFileSync('./config/timetable-body.json', JSON.stringify(timetable_body, null, "\t"));
+        fs.writeFileSync('./config/timetable-body-n.json', JSON.stringify(timetable_body_n, null, "\t"));
+
         console.log("Succes while configuring body");
+
       }catch(err){
+
         console.log("Error while configuring body");
         console.log(err);
+
       }
     
 }
 
 async function parseRawData(){
+    //TODO problem
+    if(fs.existsSync('./data/timetable_today.json') && fs.existsSync('./data/timetable_week.json') && fs.existsSync('./data/timetable_next_week.json')){
 
-    if(fs.existsSync('./data/timetable_today.json') && fs.existsSync('./data/timetable_week.json')){
         console.log("Found old files, renaming it!")
+
         fs.renameSync('./data/timetable_today.json', './data/timetable_today_old.json');
         fs.renameSync('./data/timetable_week.json', './data/timetable_week_old.json');
+        fs.renameSync('./data/timetable_next_week.json', './data/timetable_next_week_old.json');
 
     }else {
         console.log("Old files not forund!")
@@ -73,10 +142,13 @@ async function parseRawData(){
     try{
     const timetable_today = {};
     const timetable_week = {}
+    const timetable_next_week = {}
     var key = 'class'
     timetable_today[key] = []
     timetable_week[key] = []
+    timetable_next_week[key] = []
 
+    //Today
     tttoday.r.ttitems[0];
     tttoday.r.ttitems.forEach(function(entry) {
         var timetable_data = {
@@ -93,6 +165,24 @@ async function parseRawData(){
     });
     fs.writeFileSync('./data/timetable_today.json', JSON.stringify(timetable_today, null, "\t"));
 
+    //Next week
+    timetable_data_n.r.ttitems[0];
+    timetable_data_n.r.ttitems.forEach(function(entry) {
+        var timetable_data = {
+
+            starttime: entry.starttime,
+            endtime: entry.endtime,
+            date: entry.date,
+            name: entry.name,
+            teacher: entry.teacherids[0],
+            classroom: entry.classroomids[0]
+            
+        }
+        timetable_next_week[key].push(timetable_data);
+    });
+    fs.writeFileSync('./data/timetable_next_week.json', JSON.stringify(timetable_next_week, null, "\t"));
+
+    //This week
     timetable_data.r.ttitems[0];
     timetable_data.r.ttitems.forEach(function(entry) {
         var timetable_data = {
@@ -108,12 +198,66 @@ async function parseRawData(){
         timetable_week[key].push(timetable_data);
     });
     fs.writeFileSync('./data/timetable_week.json', JSON.stringify(timetable_week, null, "\t"));
+
     console.log("Parsing successfull!")
     return true;
 }catch(err){
     console.log("An error occured " + err);
     return false;
 }
-    
 }
-module.exports.getData = getData;
+async function parseRawDataToVariable(json){
+    const timetable = {};
+    var key = 'class'
+    timetable[key] = []
+
+    json.r.ttitems[0];
+    json.r.ttitems.forEach(function(entry) {
+        var timetable_data = {
+
+            starttime: entry.starttime,
+            endtime: entry.endtime,
+            date: entry.date,
+            name: entry.name,
+            teacher: entry.teacherids[0],
+            classroom: entry.classroomids[0]
+
+        }
+        timetable[key].push(timetable_data);
+    });
+
+    return timetable
+
+}
+async function getDayTT(date){
+    let dayTT;
+    let body = {
+        "__args": [
+            null,
+            {
+                "year": config.year,
+                "datefrom": date,
+                "dateto": date,
+                "table": "classes",
+                "id": "-53",
+                "showColors": true,
+                "showIgroupsInClasses": false,
+                "showOrig": true,
+                "log_module": "CurrentTTView"
+            }
+        ],
+        "__gsh": "00000000"
+    }
+    response = await fetch('https://fsre.edupage.org/timetable/server/currenttt.js?__func=curentttGetData', {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    tttoday = await response.json();
+    return await parseRawDataToVariable(tttoday);
+}
+module.exports = {
+    getData,
+    getDayTT
+};
