69b355e2f86dc856a8f22bec5e02eb3236914420
Mijo Galic <gmijo47@gmail.com>
2022-11-30 17:35:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,10 +15,7 @@ let dataConf = JSON.parse(fs.readFileSync('./config/data.json', 'utf8'));
 var days = ['Nedelja', 'Ponedeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota']
 var starttime = new Date()
     //Create client with local auth
-const client = new Client({
-    authStrategy: new LocalAuth()
-},
-{puppeteer: {args: ["--no-sandbox"]}});
+const client = new Client({puppeteer: {args: ["--no-sandbox"]}});
 
 //Generate qr code for sign in
 client.on('qr', qr => {
